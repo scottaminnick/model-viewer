144960acab6446cc4cc4b5a5e3fc2def302e0e81
scottaminnick <scott.minnick@gmail.com>
2026-03-05 02:34:35 -0600

    Update definitions.py

--- products/definitions.py
+++ products/definitions.py
@@ -414,7 +414,7 @@ register(_LLTI(
     model_id="rap13", product_id="llti",
     label="Low-Level Turbulence Index", units="index",
     herbie_model="rap", herbie_product="awp130pgrb",
-    sfc_product="wrfmsl",
+    sfc_product="wrfnat",
     searches=[],
     cmap=_llti_cmap, norm=_llti_norm, legend=_llti_legend,
 ))
